efa839031fb823b2eb399165db8c0b9500ce7a20
IlyaPidonenko <132890626+IlyaPidonenko@users.noreply.github.com>
2023-11-15 23:35:10 +0100

    Update websocketserver.js

--- server/websocketserver.js
+++ server/websocketserver.js
@@ -1,5 +1,7 @@
 const WebSocket = require("ws");
 
+let users = new Set();
+
 // Intiiate the websocket server
 const initializeWebsocketServer = (server) => {
   const websocketServer = new WebSocket.Server({ server });
@@ -9,13 +11,39 @@ const initializeWebsocketServer = (server) => {
 // If a new connection is established, the onConnection function is called
 const onConnection = (ws) => {
   console.log("New websocket connection");
+
   ws.on("message", (message) => onMessage(ws, message));
 };
 
 // If a new message is received, the onMessage function is called
 const onMessage = (ws, message) => {
   console.log("Message received: " + message);
-  ws.send("Hello, you sent -> " + message);
+  try {
+    const data = JSON.parse(message);
+
+    if (data.type === "join" && data.username) {
+      // Füge den Benutzernamen zur Benutzerliste hinzu
+      users.add(data.username);
+      // Sende die aktualisierte Benutzerliste an alle Clients
+      broadcastUserList();
+    } else {
+      // Hier kannst du weitere Nachrichtenverarbeitung hinzufügen, falls benötigt
+    }
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+  }
+};
+
+const broadcastUserList = () => {
+  const userList = Array.from(users);
+  const message = JSON.stringify({ type: "userList", userList });
+
+  // Sende die Benutzerliste an alle verbundenen Clients
+  websocketServer.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
 };
 
 module.exports = { initializeWebsocketServer };
